021a01f1d4af13210b435168c8d624815a493326
richtech9 <richwon426@gmail.com>
2022-01-18 13:33:01 +0600

    some update

--- src/components/landing/Banner/Banner.jsx
+++ src/components/landing/Banner/Banner.jsx
@@ -39,7 +39,7 @@ export const Banner = () => {
               <img
                 src={v.img}
                 alt="slider image"
-                style={{ width: "100%", height: "100%" }}
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
               />
             </div>
           ))}
